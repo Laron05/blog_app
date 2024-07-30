699aba6de8062ed21fc201a93b8f39e59ea6fada
Laron <laronenowegbe@gmail.com>
2024-07-30 08:32:20 +0100

    comit successful

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'app_blog')]
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    #[Route('/blog', name: 'app_blog')]
+    public function text(): Response
+    {
+        return new Response(content: '<h1>BLOG APPLICATION</h1>');
+    }
+}
